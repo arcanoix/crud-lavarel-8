4bfdce20eba7a1e3cfbee04bf607052ccae87ded
Gustavo Herrera <gustavoh.2312@gmail.com>
2020-12-14 14:25:26 -0500

    add relationship process

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -12,4 +13,10 @@ class Product extends Model
     protected $table = 'products';
 
     protected $guarded = [];
+
+
+    public function Orders() : HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
 }
